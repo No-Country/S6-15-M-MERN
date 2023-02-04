66da8d9f18d53ffdcf11e9a5f89412864a4735a3
FelipePadillaM <90018159+FelipePadillaM@users.noreply.github.com>
2023-02-04 16:30:41 -0500

    change port docker

--- server/dist/app.js
+++ server/dist/app.js
@@ -6,9 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to myAPI' });
+});
 app.listen(PORT, () => {
     console.log(`Server started at port: ${PORT}`);
 });
