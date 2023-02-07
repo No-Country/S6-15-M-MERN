49f4970239cacd43ab485578bfaa914159b6a598
Emiliano Pérez <emi.esp86@gmail.com>
2023-02-07 00:19:15 -0300

    review cards, primera prueba

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import NavBar from './components/NavBar';
+import Reviews from './components/Reviews';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
- 
-    <div className='bg-background-color'>
-         <NavBar/>
-      <h1 className="text-3xl font-bold underline">
-        PROBANDO
-      </h1>
-    </div>
-
-  )
+    <>
+      <div className='bg-background-color'>
+        <NavBar />
+        <h1 className='text-3xl font-bold underline'>PROBANDO</h1>
+      </div>
+      <Reviews />
+    </>
+  );
 }
 
-export default App
+export default App;
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     colors: {
-      'background-color':'#F5F5F5',
-      // Configure your color palette here
+      'background-color': '#F5F5F5',
+      'background-card-color': '#F5F7FA',
     },
     extend: {},
   },
   plugins: [],
-}
+};
