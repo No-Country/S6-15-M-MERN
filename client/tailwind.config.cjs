b66ec140f8c1a99aaeb93892744ddc07c1732c11
Emiliano Pérez <emi.esp86@gmail.com>
2023-02-08 14:34:30 -0300

    carousel 2

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,21 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    // colors: {
-    //   'backgroundColor':'#F5F5F5',
-
-    //   // Configure your color palette here
-    // },
-
-
-    // colors: {
-    //   'background-color':'#F5F5F5',
-    //   'primary':'#28315C',
-    //   'btn-color':'#7CC298',
-    //   'text-white': ' #FFFFFF',
-    //   'background-card-color': '#F5F7FA',
-    // },
+   
     theme: {
       extend: {
         colors: {
@@ -29,9 +15,7 @@ module.exports = {
     },
     fontFamily: {
       khula: ['Khula', 'sans-serif']
-
     },
     extend: {},
-  },
-  plugins: [require('tailwind-scrollbar-hide')],
+    plugins: [require('tailwind-scrollbar-hide')],
 };
