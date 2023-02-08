1e071746e764753212d72667f7b93613106ef23f
Agustin Galvan <93547064+Agustingalvan02@users.noreply.github.com>
2023-02-08 14:36:40 -0300

    Fix

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,21 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    // colors: {
-    //   'backgroundColor':'#F5F5F5',
-
-    //   // Configure your color palette here
-    // },
-
-
-    // colors: {
-    //   'background-color':'#F5F5F5',
-    //   'primary':'#28315C',
-    //   'btn-color':'#7CC298',
-    //   'text-white': ' #FFFFFF',
-    //   'background-card-color': '#F5F7FA',
-    // },
+ 
     theme: {
       extend: {
         colors: {
@@ -32,6 +18,6 @@ module.exports = {
 
     },
     extend: {},
-  },
+  
   plugins: [],
 };
