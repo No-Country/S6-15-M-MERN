48fd754d064eaf51592abc1ffe1d8d78b26dc6ba
lalomax <lalomax365@gmail.com>
2023-02-08 11:43:39 -0400

    colores personalizados tailwind
    Ajuste del archivo tailwind.config.cjs para configuraciones de color personalizadas

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -4,19 +4,30 @@ module.exports = {
   theme: {
     // colors: {
     //   'backgroundColor':'#F5F5F5',
-      
+
     //   // Configure your color palette here
     // },
-    
-    colors: {
-      'background-color':'#F5F5F5',
-      'primary':'#28315C',
-      'btn-color':'#7CC298',
-      'text-white': ' #FFFFFF',
-      'background-card-color': '#F5F7FA',
+
+    // colors: {
+    //   'background-color':'#F5F5F5',
+    //   'primary':'#28315C',
+    //   'btn-color':'#7CC298',
+    //   'text-white': ' #FFFFFF',
+    //   'background-card-color': '#F5F7FA',
+    // },
+    theme: {
+      extend: {
+        colors: {
+            'background-color':'#F5F5F5',
+            'blox':'#28315C',
+            'btn-color':'#7CC298',
+            'text-white': ' #FFFFFF',
+            'background-card-color': '#F5F7FA',
+        }
+      },
     },
-    fontFamily:{
-      khula:[ 'Khula', 'sans-serif']
+    fontFamily: {
+      khula: ['Khula', 'sans-serif']
     },
     extend: {},
   },
