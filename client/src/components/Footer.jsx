9fd551d82437aed1fd31428d5cb61f39aca32a07
Melany1999 <meelressler1999@gmail.com>
2023-02-12 11:22:34 -0300

    footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,9 +6,9 @@ import Contrata from "../assets/Marcaempresa.png"
 export default function Footer(){
     return(
      <div>
-        <footer className="flex flex-col items-center p-[64px 24px 8px] w-[1506px] h-[303px] rounded-none flex-none order-4 grow-0">
+        <footer className="flex flex-col items-center p-[64px 24px 8px] w-[100%] h-screen rounded-none flex-none order-4 grow-0">
         <div className="flex justify-end">
-            <div className="columns-3 flex flex-row items-start p-4 gap-8 absolute w-[600px] h-[135px] left-[721px] top-0px rounded-none">
+            <div className="columns-3 flex flex-row items-start absolute p-4 gap-8  w-[100%] h-screen left-[721px] top-[3273px] rounded-none">
                 <ul>
                 <li className="font-family['Gilroy'] not-italic font-extrabold text-xs text-black 600 leading-8" >SOBRE NOSOTROS</li>
                     <li className="text-sm font-family['Gilroy-medium'] leading-[150%]">Que hacemos</li>
@@ -35,18 +35,18 @@ export default function Footer(){
 
           </div>
           
-          <div className="flex flex-row items-center p-8 gap-16 absolute left-[1.5px] top-[3200px]">
+          <div className="flex flex-row items-center p-8 gap-16 absolute w-[100%] left-[1.5px] top-[3200px]">
            <img src={Contrata}/>
           </div>
 
           <div className="columns-3">
-          <ul className="flex flex-row items-center p-10 gap-16 absolute w-[229.4px] h-[51px] left-[1.5px] top-[3273px]">
+          <ul className="flex flex-row items-center p-10 gap-16 absolute w-[100%] h-[51px] left-[1.5px] top-[3273px]">
                 <li><FontAwesomeIcon icon={faFacebook} className="absolute w-12 h-12"></FontAwesomeIcon></li>
                 <li><FontAwesomeIcon icon={faInstagram} className="absolute w-12 h-12"></FontAwesomeIcon></li>
                 <li><FontAwesomeIcon icon={faTwitter} className="absolute w-12 h-12"></FontAwesomeIcon></li>
             </ul>
           </div>
-          <div className="flex flex-row items-center p-10 gap-16 absolute bottom-px left-[540px] top-[760px] text-gray-600">  
+          <div className="flex flex-row items-center p-10 gap-16 absolute w-[100%] bottom-0px left-[540px] top-[3380px] text-gray-600">  
             <span>Copyright © 2023 Contratá</span>
           </div>
         </footer>
