795cded3e60c30c0c1bcffb32d16bf558fa3c8ff
cerveux <diegomartinguzman@hotmail.com.ar>
2023-02-14 00:18:56 -0300

    fast modal

---
+++ client/src/components/Modal.jsx
@@ -0,0 +1,22 @@
+
+function Modal({children, isOpen, closeModal}) {
+  if(isOpen){
+    document.body.style.overflow = 'hidden';
+  }
+  if(!isOpen){
+    document.body.style.overflow = 'unset';
+  }
+  
+     return ( 
+        <article className={` fixed left-0 top-0 right-0 bottom-0 bg-black bg-opacity-50 items-center justify-items-center ${isOpen? "flex":"hidden"}`} onClick={closeModal}>
+  
+        <div className="flex mx-auto" onClick={(e)=>e.stopPropagation()}>
+          
+          {children}
+
+        </div>
+      </article>
+     );
+}
+
+export default Modal;
