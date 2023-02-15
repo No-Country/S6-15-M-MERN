d80bc579dd9b6e55550197072541915d51524152
Melany1999 <meelressler1999@gmail.com>
2023-02-15 19:03:42 -0300

    ultimos cambios

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,52 +4,51 @@ import Contrata from "../assets/Marcaempresa.png"
 
 
 export default function Footer(){
-    return(
-     <div>
-        <footer className="flex flex-col items-center p-[64px 24px 8px] w-[100%] h-screen rounded-none flex-none order-4 grow-0">
-        <div className="flex justify-end">
-            <div className="columns-3 flex flex-row items-start absolute p-4 gap-8  w-[100%] h-screen left-[721px] top-[3273px] rounded-none">
-                <ul>
-                <li className="font-family['Gilroy'] not-italic font-extrabold text-xs text-black 600 leading-8" >SOBRE NOSOTROS</li>
-                    <li className="text-sm font-family['Gilroy-medium'] leading-[150%]">Que hacemos</li>
-                    <li className="text-sm font-family['Gilroy-medium'] leading-[150%]">Servicios</li>
-                    <li className="text-sm font-family['Gilroy-medium'] leading-[150%]">FAQ's</li>
-                </ul>
-                <br/>
+  return(
+    <div>
+      <footer className="flex flex-col justify-between relative items-center m-0 p-[64px 24px 8px] w-[100%] top[3299.25px] bottom-0 left-[3px] rounded-none bg-red-600">
+        <div className="flex justify-start">
+          <div className="items-start mr-[150px]">
 
-                <ul>
-                    <li class="font-family['Gilroy'] not-italic font-extrabold text-xs text-black 600 leading-8">RECURSOS</li>
-                    <li className="text-sm font-family['Gilroy-medium'] leading-[150%] text-black">Blog de consejos para tu 
-                    <br/>hogar</li>
-                    <li className="text-sm font-family-['Gilroy-medium'] leading-[150%]">Contáctanos</li>
-                </ul>
-                <br/>
+              <img className="flex flex-col p-0 gap[50px] w-[320px] h-[61.28px] left-0 top-0 rounded-none" src={Contrata}/>
 
-                <ul>
-                    <li className="font-family['Gilroy'] not-italic font-extrabold text-xs text-black 600 leading-8">SOBRE NOSOTROS</li>
-                    <li className="text-sm font-family['Gilroy-medium'] leading-[150%] text-black" >Terminos & condiciones del 
-                    <br/>servicio</li>
-                    <li className="text-sm font-family['Gilroy-medium'] leading-[150%] text-black">Política de privacidad</li>
-                </ul>
-            </div>
-
-          </div>
-          
-          <div className="flex flex-row items-center p-8 gap-16 absolute w-[100%] left-[1.5px] top-[3200px]">
-           <img src={Contrata}/>
-          </div>
-
-          <div className="columns-3">
-          <ul className="flex flex-row items-center p-10 gap-16 absolute w-[100%] h-[51px] left-[1.5px] top-[3273px]">
-                <li><FontAwesomeIcon icon={faFacebook} className="absolute w-12 h-12"></FontAwesomeIcon></li>
-                <li><FontAwesomeIcon icon={faInstagram} className="absolute w-12 h-12"></FontAwesomeIcon></li>
-                <li><FontAwesomeIcon icon={faTwitter} className="absolute w-12 h-12"></FontAwesomeIcon></li>
+            <ul className="flex flex-row  md sm p-[0px] gap-[20px] left-[1.5px] top-[106.38px] mt-10">
+              <li><FontAwesomeIcon icon={faFacebook} className="w-[50px] h-[51px]"></FontAwesomeIcon></li>
+              <li><FontAwesomeIcon icon={faInstagram} className="w-[50.4px] h-[50.4px]"></FontAwesomeIcon></li>
+              <li><FontAwesomeIcon icon={faTwitter} className="w-[49px] h-[42px]"></FontAwesomeIcon></li>
             </ul>
           </div>
-          <div className="flex flex-row items-center p-10 gap-16 absolute w-[100%] bottom-0px left-[540px] top-[3380px] text-gray-600">  
-            <span>Copyright © 2023 Contratá</span>
+
+        <div className="columns-3 flex flex-row items-start py-8 p-[16px 0px 0px] gap-8 left-[721px] top-[0px] rounded-none ml-[150px] sm md lg xl">
+            <ul>
+              <li className="font-family['Gilroy'] not-italic font-extrabold text-xs text-black 600 leading-8" >SOBRE NOSOTROS</li>
+              <li className="text-sm font-family['Gilroy-medium'] leading-[150%]">Que hacemos</li>
+              <li className="text-sm font-family['Gilroy-medium'] leading-[150%]">Servicios</li>
+              <li className="text-sm font-family['Gilroy-medium'] leading-[150%]">FAQ's</li>
+            </ul>
+              <br/>
+
+            <ul>
+              <li class="font-family['Gilroy'] not-italic font-extrabold text-xs text-black 600 leading-8">RECURSOS</li>
+              <li className="text-sm font-family['Gilroy-medium'] leading-[150%] text-black">Blog de consejos para tu 
+              <br/>hogar</li>
+              <li className="text-sm font-family-['Gilroy-medium'] leading-[150%]">Contáctanos</li>
+            </ul>
+              <br/>
+
+            <ul>
+              <li className="font-family['Gilroy'] not-italic font-extrabold text-xs text-black 600 leading-8">SOBRE NOSOTROS</li>
+              <li className="text-sm font-family['Gilroy-medium'] leading-[150%] text-black" >Terminos & condiciones del 
+              <br/>servicio</li>
+              <li className="text-sm font-family['Gilroy-medium'] leading-[150%] text-black">Política de privacidad</li>
+            </ul>
           </div>
-        </footer>
-     </div>
-    )
+        </div>
+
+          <div className="flex flex-row items-center justify-center p-[16px 0px] gap-8 mt-[20px] text-gray-600">  
+            <span className="mr-[45px]">Copyright © 2023 Contratá</span>
+          </div>
+      </footer>
+    </div>
+  )
 }
