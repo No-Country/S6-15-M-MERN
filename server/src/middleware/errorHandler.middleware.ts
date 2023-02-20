e49e5247df0e635a5b1575802c5f1b30b1fdf549
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2023-02-20 18:20:59 +0100

    cors, mongo warning fixed

--- server/src/middleware/errorHandler.middleware.ts
+++ server/src/middleware/errorHandler.middleware.ts
@@ -2,7 +2,6 @@ import { Response, Request, NextFunction } from "express";
 import { AppError } from "../utils/errorObjectExtended";
 
 const errorHandler = (error: AppError, _req: Request, res: Response, _next: NextFunction) => {
-  console.log("PASA POR ERROR HANDLER");
   res.header("Content-Type", "application/json");
 
   const statusCode = error.statusCode || 500;
