6327a99afa9048fb1de886c67dbd9a9c4924bfeb
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2023-02-20 15:22:45 +0100

    googlecontroller

---
+++ server/src/controllers/google.controller.ts
@@ -0,0 +1,9 @@
+import { Request , Response } from "express";
+
+/* registro de usuario */
+const googleLoginCtrl = async ({body}:Request, res: Response) => {
+    const data = body;
+    res.status(200).json({data: data});
+};
+
+export {googleLoginCtrl}
