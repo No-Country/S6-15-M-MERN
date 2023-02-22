fc70b322fc41a45dd5ade330bc79e9aa2d40bf77
cerveux <diegomartinguzman@hotmail.com.ar>
2023-02-21 21:20:51 -0300

    services working with backend

--- client/src/components/ServicesCards.jsx
+++ client/src/components/ServicesCards.jsx
@@ -1,15 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Card from "./MostRequested/Card";
 import data from "./MostRequested/data.json"
+import { useApi } from '../hooks/useApi';
 
 function ServicesCards() {
 
+    const [servicios, setServicios] = useApi();
     const [busqueda, setBusqueda] = useState("Todos");
+
+    useEffect(() => {
+        setServicios()
+    }, [])
+    
 
     const handleOption = (event)=>{
         const election = event.target.value;
         setBusqueda(election);
-        console.log(election)
 
     }
     return (
@@ -24,36 +30,49 @@ function ServicesCards() {
                 </p>
 
                 <select class="text-base mx-auto mt-16 font-normal rounded-full border-2 bg-green-100 text-gray-600 h-12 w-[429px] pl-5 pr-10  hover:border-gray-400 focus:outline-none appearance-none">
+            
+            
             <option onClick={handleOption}>Todos</option>
-            <option onClick={handleOption}>Lavado de autos</option>
-            <option  onClick={handleOption}>Mecanico</option>
-            <option  onClick={handleOption}>Electricista</option>
-            <option  onClick={handleOption}>Plomero</option>
-            <option  onClick={handleOption}>Masajista</option>
-            <option  onClick={handleOption}>Contadora</option>
-            <option  onClick={handleOption}>Jardinero</option>
-        </select>
-            </div>
-            <div className="flex justify-center flex-wrap gap-5 max-w-[1300px] mt-5 mx-auto pt-10">
-                {(busqueda === "Todos") &&
-                data.resources.map((requested, index) => {
+
+            {
+                servicios !== null &&
+                servicios.jobs.map((requested) => {
                     return (
-                        <div className="my-4" key={index}>
-                            <Card imagen={requested.imageUrl} title={requested.title} description={requested.descripcion} price={requested.price} />
-                        </div>
+                        <option key={requested._id} onClick={handleOption}>{requested.service}</option>
 
                     )
                 })
+            }
+        </select>
+            </div>
+            <div className="flex justify-center flex-wrap gap-5 max-w-[1300px] mt-5 mx-auto pt-10">
+                
+                {
+                    servicios === null ?
+                     <h2 className="text-[#28315C] animate-bounce text-xl" >Loading</h2> :
+                     ((busqueda === "Todos") &&
+                    servicios.jobs.map((requested, index) => {
+                        return (
+                            <div className="my-4" key={requested._id}>
+                                <Card imagen={requested.jobImageUrl} title={requested.service} description={requested.description}  />
+                            </div>
+    
+                        )
+                    })
 
+                    
+                    )
+                
+           
              
                 
                 }
                 {(busqueda !== "Todos") &&
-                data.resources.map((requested, index) => {
-                    if(requested.title === busqueda){
+                servicios.jobs.map((requested) => {
+                    if(requested.service === busqueda){
                         return (
-                            <div className="my-4" key={index}>
-                                <Card imagen={requested.imageUrl} title={requested.title} description={requested.descripcion} price={requested.price} />
+                            <div className="my-4" key={requested._id}>
+                                <Card imagen={requested.jobImageUrl} title={requested.service} description={requested.description}  />
                             </div>
     
                         )
@@ -66,6 +85,7 @@ function ServicesCards() {
                 
                 }
             </div>
+            
 
         </>
     );
