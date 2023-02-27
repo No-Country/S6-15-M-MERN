17b41a36445e3d3fc87f138bb3d97a71d581db3d
FelipePadillaM <90018159+FelipePadillaM@users.noreply.github.com>
2023-02-27 10:56:16 -0500

    Adding readme and bannerImage to jobs

--- server/src/models/job.model.ts
+++ server/src/models/job.model.ts
@@ -5,7 +5,8 @@ const JobsSchema: Schema = new Schema({
   service: { type: String, required: true, unique: true },
   title: { type: String, required: true },
   description: { type: String, required: true },
-  jobImageUrl: { type: String }
+  jobImageUrl: { type: String },
+  jobBannerUrl: {type: String}
 });
 
 export default mongoose.model<IJobModel>("Jobs", JobsSchema);
