54a2592db830c6cd1ea62b5eb0ecb9b002b5123f
cerveux <diegomartinguzman@hotmail.com.ar>
2023-02-28 04:33:41 -0300

    perfil profesional conectado al back

--- client/src/components/services/CardServices.jsx
+++ client/src/components/services/CardServices.jsx
@@ -1,38 +1,32 @@
-import React from 'react'
-import { useSearchParams, useNavigate } from 'react-router-dom'
-import profile from "../../assets/profile.jpeg"
-import './CardService.css'
+import React from 'react';
+import profile from '../../assets/profile.jpeg';
+import './CardService.css';
 
-export const CardServices = ({id, title, image, description, service}) => {
+export const CardServices = ({ title, image, description, service, id }) => {
 
 
- 
-
-  /* const [searchparams] = useSearchParams(); */
-
-  const navigate = useNavigate();
-
-  function handleredirect() {
-    navigate(`/perfilProfesional/${id}`);
-    console.log(item._id);
-  }
 
   return (
-      <div onClick={handleredirect} className='containerCard relative'>
-        <img src={image}/> 
-       
-        <img className=' inset-0 rounded-full w-20 max-h-20 absolute ml-[42%] mt-16 border-[#28315C] border-solid border-2 ' src={profile} alt="" />
-            <div className='cardDiv'>
-              <div className='serviceDiv'>
-                <h4> {service}</h4>
-                <p className='pCard'>{title}</p>
-              </div>
-                <div className='divTetx'>
-                  <div className='divPrice'>
-                   <p>{description}</p> 
-                  </div>
-                </div>
-            </div>    
+    <div className='containerCard relative'>
+      <img src={image} />
+
+      <img
+        className=' inset-0 rounded-full w-20 max-h-20 absolute ml-[42%] mt-16 border-[#28315C] border-solid border-2 '
+        src={profile}
+        alt=''
+      />
+      <div className='cardDiv' id={id}>
+        <div className='serviceDiv'>
+          <h4> {service}</h4>
+          <p className='pCard'>{title}</p>
+        </div>
+        <div className='divTetx'>
+          <div className='divPrice'>
+            <p>{description}</p>
+            {/* <h5 className='text-buttons-buttonGreen font-black'> $price</h5> */}
+          </div>
+        </div>
       </div>
-  )
-}
+    </div>
+  );
+};
--- client/src/screens/ServicesDetail.jsx
+++ client/src/screens/ServicesDetail.jsx
@@ -1,45 +1,55 @@
 import React, { useEffect, useState } from 'react';
-import { useSearchParams, useNavigate } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import { CardServices } from '../components/services/CardServices';
 import Dropdown from '../components/services/Dropdown';
 import { useSelector } from 'react-redux';
 import { useApi } from '../hooks/useApi';
-/* import image from "../assets/Frame.png" */
+import image from '../assets/Frame.png';
+import { useNavigate } from 'react-router-dom';
 
 export const ServicesDetail = ({ props }) => {
-  const [searchparams] = useSearchParams();
+  const [searchparams] = useSearchParams(); 
   const [, , , professionalsList] = useApi();
-  const [professional, setProfessional] = useState(null);
-  const [ciudad, setCiudad] = useState("")
+  const [professional, setProfessional] = useState([]);
+  const [ciudad, setCiudad] = useState('');
+  const [imagen, setImagen] = useState("https://firebasestorage.googleapis.com/v0/b/db-demo-e7d23.appspot.com/o/banners%2Felectricista.png?alt=media&token=41be896c-0152-4d4c-a6c4-79f14258b954")
 
-  const handleOption = (e)=>{
-    (e.target.value === "Todos") ?
-    setCiudad(""):
-    setCiudad(e.target.value)
-
-  }
-
- 
-
-
-  
+  const navigate = useNavigate();
+  const handleOption = (e) => {
+    e.target.value === 'Todos' ? setCiudad('') : setCiudad(e.target.value);
+  };
 
   const professionals = useSelector((state) => state.professionals);
-  const jobs = useSelector((state) => state.jobs);
+  const jobs = useSelector(state => state.jobs)
 
+  const [busqueda, setBusqueda] = useState('Todos');
 
-  const [busqueda, setBusqueda] = useState('Todos'); 
+  useEffect(()=>{
+    if(jobs.jobs.length > 1){
+      setImagen(jobs.jobs.filter(job => job._id === searchparams.get('id'))[0].jobBannerUrl)
+      
 
+    }
 
-
-
+  }, [jobs])
 
 
   useEffect(() => {
-    setProfessional({ id: searchparams.get('id') });
-    professionalsList(searchparams.get("id"), ciudad);
+    /* setProfessional({ id: searchparams.get('id') }); */
+    professionalsList(searchparams.get('id'), ciudad);
   }, [ciudad]);
 
+  function handleOnClick(item) {
+   /*  navigate(
+      {
+        pathname: '/perfilProfesional',
+        search: createSearchParams({
+          id: id,
+        }).toString(),
+      }
+    ) */
+    navigate(`/perfilProfesional?id=${item._id}`);
+  }
 
   return (
     <>
@@ -69,45 +79,26 @@ export const ServicesDetail = ({ props }) => {
               {/* agrego esta porcion de código solo para probarlo rápido */}
 
               <select onClick={handleOption} className='text-base mx-auto mt-16 font-normal rounded-full border-2 bg-green-100 text-gray-600 h-12 w-[429px] pl-5 pr-10  hover:border-gray-400 focus:outline-none appearance-none'>
-          <option >Todos</option>
+                <option >Todos</option>
 
-          <option  >
-          Cucuta
-                </option>
-        </select>
-
-
-
-
-
-
-
-
-
-
-
-
+                <option >Cucuta</option>
+              </select>
 
               {/* aqui termina el agregado configurarlo con el dropdown */}
-
 
               {/* <Dropdown /> */}
             </div>
           </div>
           <div className=' p-2 flex flex-wrap gap-5 items-center justify-center '>
-            {
-            professionals.professionals.map((item, index) => {
-              
+            {professionals.professionals.map((item, index) => {
               return (
                 <div
-                  key={index}
+                  key={item._id}
                   className='flex max-sm:flex-col max-sm:items-center'
+                  onClick={() => handleOnClick(item)}
                 >
-                  <CardServices                  
-                    image={professional !== null &&
-                      jobs.jobs.filter(job =>job._id === professional.id)[0].jobBannerUrl
-                    }
-                    
+                  <CardServices
+                    image={imagen}
                     description={item.description}
                     service={item.name}
                     id={item._id}
