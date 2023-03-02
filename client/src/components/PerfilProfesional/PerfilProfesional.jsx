b9e0fdfa4826b29a8b7e5421dff70209399b583c
Emiliano Pérez <emi.esp86@gmail.com>
2023-03-02 01:53:09 -0300

    ctrl z

--- client/src/components/PerfilProfesional/PerfilProfesional.jsx
+++ client/src/components/PerfilProfesional/PerfilProfesional.jsx
@@ -2,7 +2,7 @@ import Foto from '../../assets/profile.jpeg';
 import Icon from '../../assets/Icon.png';
 import Telefono from '../../assets/Telefono.png';
 import Correo from '../../assets/Correo.png';
-import envelope from "../../assets/envelope.png"
+import envelope from '../../assets/envelope.png';
 import { opcionesApp } from '../../utils/opcionesApp';
 
 import { useSearchParams, useParams, useNavigate } from 'react-router-dom';
@@ -11,74 +11,55 @@ import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faUpload } from '@fortawesome/free-solid-svg-icons';
-
+import { faUpload } from '@fortawesome/free-solid-svg-icons';
 
 export default function PerfilProfesional() {
+  const profile = useSelector((state) => state.profile);
+  const jobs = useSelector((state) => state.jobs);
 
-  const profile = useSelector(state => state.profile)
-  const jobs = useSelector(state => state.jobs)
+  const { returnJob, returnBanners } = opcionesApp();
 
-  const {returnJob, returnBanners} = opcionesApp();
+  const [imagen, setImagen] = useState(
+    'https://firebasestorage.googleapis.com/v0/b/db-demo-e7d23.appspot.com/o/banners%2Felectricista.png?alt=media&token=41be896c-0152-4d4c-a6c4-79f14258b954'
+  );
 
-
-  const [imagen, setImagen] = useState("https://firebasestorage.googleapis.com/v0/b/db-demo-e7d23.appspot.com/o/banners%2Felectricista.png?alt=media&token=41be896c-0152-4d4c-a6c4-79f14258b954")
-
-
-  const [readJobs, , , , getProfessional] = useApi()
+  const [readJobs, , , , getProfessional] = useApi();
 
   const [searchparams] = useSearchParams();
 
   /* {profesionalId === JSON.parse(localStorage.getItem("user")).id && console.log("Si")} */
 
-
-
-
-  const profesionalId = (searchparams.get('id'));
- /*  console.log(profesionalId);
+  const profesionalId = searchparams.get('id');
+  /*  console.log(profesionalId);
   console.log(JSON.parse(localStorage.getItem("user")).id);
  */
-  
-
 
   useEffect(() => {
-    readJobs()
+    readJobs();
     getProfessional(profesionalId);
-
-
-
-  }, [])
+  }, []);
 
   useEffect(() => {
-
     setTimeout(() => {
-      if ((jobs.jobs.filter(job => job._id === profile.profile.user.job)[0] !== undefined) && (profile.profile.user !== undefined)) {
-
-        setImagen(jobs.jobs.filter(job => job._id === profile.profile.user.job)[0].jobBannerUrl)
-
-
-
+      if (
+        jobs.jobs.filter((job) => job._id === profile.profile.user.job)[0] !==
+          undefined &&
+        profile.profile.user !== undefined
+      ) {
+        setImagen(
+          jobs.jobs.filter((job) => job._id === profile.profile.user.job)[0]
+            .jobBannerUrl
+        );
       }
-
-    }, 1000)
-
-
-
-
-  })
+    }, 1000);
+  });
 
   const navigate = useNavigate();
 
-  const sendMessage = ()=>{
+  const sendMessage = () => {
     const email = profile.profile.user.email;
-    navigate("/sol-servicio", {state: { email }})
-
-  }
-
-
-
-
-
+    navigate('/sol-servicio', { state: { email } });
+  };
 
   return (
     <div>
@@ -90,81 +71,70 @@ export default function PerfilProfesional() {
         className='w-full height-[146px] flex-none order-1 self-stretch grow-0'
         src={returnBanners(profile.profile.user.job)}
       />
-      <div className="container mx-auto  ">
+      <div className='container mx-auto  '>
         <div className='flex w-full'>
-          <div
-            className=''>
-
+          <div className=''>
             <div className='relative top-[-55px] flex flex-col'>
               <img
-                className=' w-64 h-64 mx-auto  rounded-full border-8 border-[#26B893] border-opacity-50 '
-                src={profile.profile.user.avatarURL !== "" ? profile.profile.user.avatarURL : Foto}
+                className='w-64 h-64 max-[1625px]:w-52 max-[1625px]:h-52 max-[1280px]:w-48 max-[1280px]:h-48 max-[1024px]:w-48 max-[1024px]:h-48 max-[768px]:w-44 max-[768px]:h-44  mx-auto rounded-full border-8 border-[#26B893] border-opacity-50 '
+                src={
+                  profile.profile.user.avatarURL !== ''
+                    ? profile.profile.user.avatarURL
+                    : Foto
+                }
               />
-              {profesionalId === JSON.parse(localStorage.getItem("user")).id &&
+              {profesionalId ===
+                JSON.parse(localStorage.getItem('user')).id && (
                 <button className='bg-[#43936c] w-9 h-9 rounded-full text-white relative left-[116px] top-[-25px]'>
                   <FontAwesomeIcon icon={faUpload} />
                 </button>
-              }
-
+              )}
 
               {/* <img
             className='relative flex flex-row justify-center items-center p-0 gap-4 left-[10px] top-[-200px] w-[204px]'
             src={Enviame}
           /> */}
-
-
             </div>
-
-
-
 
             <div className='flex flex-row  '>
               <img
-                className=" ml-4"/* 'relative left-[15px] right-[12.5%] top-[-250px] bottom-[12.5%]' */
+                className=' ml-4' /* 'relative left-[15px] right-[12.5%] top-[-250px] bottom-[12.5%]' */
                 src={Telefono}
               />
-              <p className="ml-4">
-                + 59 1 1001 8830
-              </p>
-
+              <p className='ml-4'>+ 59 1 1001 8830</p>
             </div>
 
-            <div className="flex flex-row items-center mt-4">
-              <img
-                className='ml-4 h-4'
-                src={Correo}
-              />
-              <p className="m-4">
-                {profile.profile.user.email}
-              </p>
-
+            <div className='flex flex-row items-center mt-4'>
+              <img className='ml-4 h-4' src={Correo} />
+              <p className='m-4'>{profile.profile.user.email}</p>
             </div>
 
-
-
-            {/* ES ACA EL BOTON */}
-
-
-
+            {/* ES ACA EL BOTONNNNNNNNN */}
 
             <div className='w-full flex justify-center'>
-              <button onClick={sendMessage} className=' bg-[#43936c] text-white py-3 px-8 rounded-lg w-full m-2 flex'  > <img src={envelope} alt="envelope" /> <p className=' ml-5'>Enviame un mensaje</p> </button>
-
+              <button
+                onClick={sendMessage}
+                className=' bg-[#43936c] text-white py-3 px-8 rounded-lg w-48 whitespace-nowrap m-2 flex'
+              >
+                {' '}
+                <img
+                  src={envelope}
+                  alt='envelope'
+                  className='ml-4 w-4 mt-[3px]'
+                />{' '}
+                <p className=' ml-4'>Mensaje</p>{' '}
+              </button>
             </div>
-
-
           </div>
           <div className='flex-col'>
-            <h1 className="font-inter mt-5  not-italic font-bold text-[39px] leading-[120%] flex items-center  ml-[100px] text-[#083A50]">
+            <h1 className='font-inter mt-5  not-italic font-bold text-[39px] leading-[120%] flex items-center  ml-[100px] text-[#083A50]'>
               {profile.profile.user.name}
             </h1>
             <p className="font-['Inter'] not-italic font-semibold text-[18px] leading-[24px] top-[-158px] ml-[100px] mt-2 text-[#083A50]">
               {returnJob(profile.profile.user.job)}
             </p>
 
-
-
-            <div className="flex mt-4">
+            <div className='flex mt-4'>
               <img
                 className=' left-[260px] right-[16.67%] top-[-148px] bottom-[7.03%] ml-[100px]'
                 src={Icon}
@@ -179,34 +149,24 @@ export default function PerfilProfesional() {
                 {profile.profile.user.description}
               </p>
             </div>
-
-
-            <div className=' mt-7'>
-              <p className=' ml-[100px] font-bold text-3xl'>Proyectos más recientes</p>
+            {/*ACA VA EL CAROUSEL???????????? */}
+            <div className=' mt-52'>
+              <p className=' ml-[100px] font-bold text-3xl'>
+                Proyectos más recientes
+              </p>
             </div>
             <div>
-
-
-              {profesionalId === JSON.parse(localStorage.getItem("user")).id &&
+              {profesionalId ===
+                JSON.parse(localStorage.getItem('user')).id && (
                 <div className=' ml-[100px]'>
-                  <button className=' w-40 h-10 bg-[#43936c] rounded-lg text-white mt-10'>Subir fotos</button>
+                  <button className=' w-40 h-10 bg-[#43936c] rounded-lg text-white mt-10'>
+                    Subir fotos
+                  </button>
                 </div>
-              }
-
-
+              )}
             </div>
-
-
-
           </div>
         </div>
-
-
-
-
-
-
-
 
         {/* <div className=''>
         <OurServices />
@@ -217,9 +177,7 @@ export default function PerfilProfesional() {
       </div> */}
 
         <div className=''></div>
-
       </div>
-
     </div>
   );
 }
