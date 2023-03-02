bada5e00ae2e6c68c504358f5b83b6aaa6948322
Maiten Cullen <maitencullen@gmail.com>
2023-03-02 03:04:23 -0300

    editProfesional

--- client/src/components/services/CardServices.jsx
+++ client/src/components/services/CardServices.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import profile from '../../assets/profile.jpeg';
 import './CardService.css';
+import marta from '../../assets/marta.png'
+import { useSelector } from 'react-redux';
 
 export const CardServices = ({ title, image, description, service, id }) => {
-
-
+  const profile = useSelector(state => state.profile)
+  console.log(profile.profile.user, " mini avatar")
 
   return (
     <div className='containerCard relative'>
@@ -12,7 +14,7 @@ export const CardServices = ({ title, image, description, service, id }) => {
 
       <img
         className=' inset-0 rounded-full w-20 max-h-20 absolute ml-[42%] mt-16 border-[#28315C] border-solid border-2 '
-        src={profile}
+        // src={!profile.profile.user.avatarURL.path? marta : profile.profile.user.avatarURL.path }
         alt=''
       />
       <div className='cardDiv' id={id}>
